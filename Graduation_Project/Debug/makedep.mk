4bbaab2229277d261b9ed51a9d1e16c13ee1bd37
A-Aref <abdallahahmadaref@gmail.com>
2023-07-30 10:37:16 +0300

    Project commit

---
+++ Graduation_Project/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC.c
+
+EEPROM.c
+
+I2C.C
+
+Interrupt.c
+
+main.c
+
+SS.c
+
+Timer0.c
+
+Timer2.c
+
